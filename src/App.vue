0df09a4a663fec84ff30a0b32ac438085c59a829
max <max>
2025-02-10 20:05:50 +0100

    feat: Implement undo functionality for cart state changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
+import { ref, reactive } from 'vue'
 import TheHeader from '@/components/TheHeader.vue'
 import ProductCard from '@/components/ProductCard.vue'
 import { useProductStore } from '@/stores/useProductStore.js'
 import { useCartStore } from '@/stores/useCartStore.js'
 const cartStore = useCartStore()
 const productStore = useProductStore()
+const history = reactive([])
+const doingHistory = ref(false)
+history.push(JSON.stringify(cartStore.$state))
+const undo = () => {
+  if (history.length === 1) return
+  doingHistory.value = true
+  history.pop()
+  cartStore.$state = JSON.parse(history.at(-1))
+  doingHistory.value = false
+}
 productStore.fill()
+
+history.push(JSON.stringify(cartStore.$state))
 
 cartStore.$onAction(({
   name,
@@ -24,14 +37,20 @@ cartStore.$onAction(({
 })
 
 cartStore.$subscribe((mutation, state) => {
-  console.log({mutation})
-  console.log({state})
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state))
+  }
 })
+
+
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
